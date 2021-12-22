da31f3650aef72dd2da27e2b5c97c39cda4e06c7
Ray's Git <lancelot53.ra@gmail.com>
2021-12-22 16:04:28 -0500

    Completed

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,35 @@
-require 'pry'
+require "pry"
 
-  # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
-  
+# This is the array we will be passing into the remove_strawberry method
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"],
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"],
+  },
+}
+
 def remove_strawberry(contacts)
-
+  contacts.each do |name, value|
+    if name == "Freddy Mercury"
+      contacts["Freddy Mercury"].each do |name|
+        #binding.pry
+        if name[0] == :favorite_ice_cream_flavors
+        #if name == :favorite_ice_cream_flavors
+          contacts["Freddy Mercury"][:favorite_ice_cream_flavors].each_with_index do |name, index|
+            if name == "strawberry"
+              contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_at(index)
+            end
+          end
+        end
+      end
+    end
+  end
 end
+
+remove_strawberry(contacts)
